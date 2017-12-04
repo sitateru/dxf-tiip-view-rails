945d8d001856cd997e43b6442667305dc6e4070e
Shinobu Izumi <stagesp1@gmail.com>
2017-12-04 23:47:35 +0000

    engine_root path

--- app/helpers/dxf_tiip_view/view_helper.rb
+++ app/helpers/dxf_tiip_view/view_helper.rb
@@ -1,7 +1,7 @@
 module DxfTiipView
     module ViewHelper
         def dxf_tiip(selector, font_file_path)
-            engine_root =  Engine.root.to_path.sub(Rails.root.to_path, '')
+            engine_root =  '/dxf-tiip-view' //Engine.root.to_path.sub(Rails.root.to_path, '')
             render partial: 'dxf_tiip_view/make_viewable', locals: { selector: selector, font_file_path: font_file_path, engine_root: engine_root }
         end
     end
