1f0f774f8c58a5f260870f5789bca2e2b0b5983a
takafujimoto <takahiro.fujimoto@sitateru.com>
2018-06-04 13:03:51 +0900

    ajaxで要素が変更された場合にDXFviewerが動かなくなるため修正 ・helperに引数を追加 ・追加した引数が存在する場合は親要素に対してイベントハンドリングを行う

--- app/helpers/dxf_tiip_view/view_helper.rb
+++ app/helpers/dxf_tiip_view/view_helper.rb
@@ -1,6 +1,6 @@
 module DxfTiipView
     module ViewHelper
-        def dxf_tiip(selector, font_file_path)
+        def dxf_tiip(selector, font_file_path, parent='')
            
             # p DxfTiipView::Engine.mounted_path.spec.right.left
             # engine_root =  '/dxf_tiip_view' #Engine.root.to_path.sub(Rails.root.to_path, '')
@@ -8,7 +8,7 @@ module DxfTiipView
             engine_root = '/'+DxfTiipView::Engine.mounted_path.spec.right.left
             # p engine_root
             
-            render partial: 'dxf_tiip_view/make_viewable', locals: { selector: selector, font_file_path: font_file_path, engine_root: engine_root }
+            render partial: 'dxf_tiip_view/make_viewable', locals: { selector: selector, font_file_path: font_file_path, engine_root: engine_root, parent: parent }
         end
     end
 end
